d3c99db2d57e4569a6be9ce124447ed21daa2c61
myname <01685839848asz@gmail.com>
2024-05-23 00:42:29 +0700

    up

--- src/redux/CallFireBase/fetchDataProducts.js
+++ src/redux/CallFireBase/fetchDataProducts.js
@@ -2,13 +2,16 @@ import { db } from "~/firebase";
 import { getDocs, collection } from "firebase/firestore";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export var fetchDataProducts = createAsyncThunk("DataProducts", async () => {
-  const doc = await getDocs(collection(db, "DataProducts"));
-  const res = [];
-  doc.forEach((item) => {
-    res.push({
-      ...item.data(),
+export var fetchDataProducts = createAsyncThunk(
+  "fetchDataProducts",
+  async () => {
+    const doc = await getDocs(collection(db, "DataProducts"));
+    const res = [];
+    doc.forEach((item) => {
+      res.push({
+        ...item.data(),
+      });
     });
-  });
-  return res;
-});
+    return res;
+  }
+);
